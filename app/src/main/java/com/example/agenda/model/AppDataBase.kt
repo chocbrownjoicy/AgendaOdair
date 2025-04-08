9ac66c35020c6b741e653b4bed2098adc6db631f
chocbrownjoicy <joicy.pereira@aluno.faculdadeimpacta.com.br>
2025-04-07 22:32:38 -0300

    Atualização 2

--- app/src/main/java/com/example/agenda/model/AppDataBase.kt
+++ app/src/main/java/com/example/agenda/model/AppDataBase.kt
@@ -1,7 +1,12 @@
 package com.example.agenda.model
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 
-@Database(entities = [Usuarios::class], ()
+@Database(entities = [Usuarios::class], version = 1 )
 class AppDataBase {
+abstract class AppDataBase:RoomDatabase(){
+    abstract fun usuarios():Usuarios
+}
+
 }
